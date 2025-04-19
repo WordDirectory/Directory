abcd44f3ec3bf1b62ca1151ce9a36d9fc713a70a
Maze Winther <mazewinther@gmail.com>
2025-04-19 22:39:41 +0200

    style: adjust header layout for improved visibility on mobile

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,15 +8,17 @@ import { ThemeToggle } from "./theme-toggle";
 export function Header() {
   return (
     <header className="w-full px-4 border-b bg-background flex h-14 items-center justify-between">
-      <div className="mr-4 hidden md:flex">
-        <Link href="/" className="mr-6 flex items-center space-x-3">
+      <div className="mr-4 flex">
+        <Link href="/" className="flex items-center space-x-3">
           <Image
             src="/logo.png"
             alt="WordDirectory Logo"
             width={32}
             height={32}
           />
-          <h2 className="text-lg font-bold sm:inline-block">WordDirectory</h2>
+          <h2 className="pr-12 text-lg font-bold sm:inline-block hidden md:block">
+            WordDirectory
+          </h2>
         </Link>
       </div>
 

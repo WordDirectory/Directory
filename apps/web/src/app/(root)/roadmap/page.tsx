952f1ed50a4f2239dac34e2d546e476ac6e1c308
Maze Winther <mazewinther@gmail.com>
2025-04-25 01:23:25 +0200

    fix(web): update roadmap item description to reflect current word count of 40,000

--- apps/web/src/app/(root)/roadmap/page.tsx
+++ apps/web/src/app/(root)/roadmap/page.tsx
@@ -23,7 +23,7 @@ const roadmapItems: {
   {
     title: "Add 150.000 words",
     description:
-      "Expect 150.000 words soon. Current count is around 20.000 but they're not live yet. Estimated completion date: May 4th, 2025.",
+      "Expect 150.000 words soon. Current count is around 40.000 but they're not live yet. Estimated completion date: May 4th, 2025.",
     status: {
       text: "In progress",
       complete: false

e58f6752fa2ae9ca71b5c03f35547d63d3fb31f7
Maze Winther <mazewinther@gmail.com>
2025-04-29 02:26:09 +0200

    style(web): adjust gap in WordHeader component for better layout

--- apps/web/src/app/(root)/words/[word]/page.tsx
+++ apps/web/src/app/(root)/words/[word]/page.tsx
@@ -93,7 +93,7 @@ function WordHeader({ word, details }: { word: string; details: TWord }) {
             className="h-20 w-20 object-contain"
           />
         )}
-        <div className="flex items-center gap-6">
+        <div className="flex items-center gap-5">
           <h1 className="text-4xl sm:text-5xl md:text-7xl font-bold tracking-tight text-foreground/85 break-words hyphens-auto max-w-full">
             {capitalize(word)}
           </h1>

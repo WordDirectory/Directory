8d581262ee47f3f8d9e8f32f8a6b65c94bcdee18
Maze Winther <mazewinther@gmail.com>
2025-05-01 02:01:14 +0200

    oops

--- apps/web/src/lib/db/index.ts
+++ apps/web/src/lib/db/index.ts
@@ -3,8 +3,7 @@ import { Pool } from "pg";
 import * as schema from "./schema";
 
 const pool = new Pool({
-  // WORKS
-  connectionString: "postgres://postgres:chakina1@localhost:5432/worddir",
+  connectionString: process.env.DATABASE_URL,
   max: 10,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,

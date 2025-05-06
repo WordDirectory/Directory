7bb31809513a0622457718ea4a4166dd279ec6d3
Maze Winther <mazewinther@gmail.com>
2025-05-07 00:58:34 +0200

    fix: next config deprection error

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -4,7 +4,12 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
   images: {
-    domains: ["images.unsplash.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
   },
 };
 
